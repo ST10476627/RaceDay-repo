9d23beaf134018773a35072985ec36a63c758c81
ST10476627 <ST10476627@rcconnect.edu.za>
2026-09-04 15:09:27 +0200

    Recreate RaceDay database and initial tables
    Recreated the RaceDay database and its associated tables with initial data inserts.

--- prog raceday.sql
+++ prog raceday.sql
@@ -5,7 +5,7 @@ RoleID int identity(1,1) primary key,
 RoleName varchar(100) not null unique
 );
 
-
+--this a the user table-- 
 CREATE TABLE [User] (
     UserId INT IDENTITY(1,1) PRIMARY KEY,
     FirstName varchar (100) NOT NULL,
